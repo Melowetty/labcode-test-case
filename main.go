aaa72f81ab6c9fc40208b508241ff95384cdabb7
Denis Malinin <melowetty@mail.ru>
2025-03-31 02:26:15 +0500

    add: camera and area controllers

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"labcode-test-case/internal/controller"
 	"net/http"
 )
 
+type Server struct {
+	cameraController *controller.CameraController
+	areaController   *controller.AreaController
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
-	})
+	server := &Server{}
+	server.cameraController = controller.NewCameraController()
+	server.areaController = controller.NewAreaController()
 
 	fmt.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", nil)
